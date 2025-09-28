a17513e5ca7ec7092a5dd657e0a4931908d9009d
Delunsss2345 <huypham1459@gmail.com>
2025-09-28 09:59:53 +0700

    up

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -12,7 +12,7 @@ const Home = () => {
           Xem Tasks
         </Link>
         <a
-          href="/public/redux.html"
+          href="/redux.html"
           target="_blank"
           rel="noopener noreferrer"
           className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 transition"
